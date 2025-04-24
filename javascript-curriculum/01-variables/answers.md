e55e39fa43d716107cadf2fc4df77f53c24d7c5a
simosd02 <simoo.sd02@gmail.com>
2025-04-24 16:44:36 +0100

    ok

---
+++ javascript-curriculum/01-variables/answers.md
@@ -0,0 +1,12 @@
+# Task 3: Reassign Values
+When we try to reassign a const variable, we see an error because the const variable does not crash anymore, because it is constant and incrasable.
+
+# ● What’s the difference between var, let, and const?
+var : Old way. Function-scoped. Can be changed and re-declared.
+let : Newer. Block-scoped. Can be changed, but not re-declared.
+const : Newer. Block-scoped. Can't be changed or re-declared.
+
+# ● When should we use const over let?
+Use const when the value shouldn't change, and use let when the value will change later.
+
+# ● Why should we avoid using var in modern JavaScript?
---
+++ javascript-curriculum/01-variables/variables-exercise.js
@@ -0,0 +1,35 @@
+var companyName = "ReFood";
+//Declare a variable using var 
+
+let internName = "John Doe";
+//Declare a variable using let
+
+const intershipYear = 2025;
+//Declare a variable using const
+
+console.log(companyName);
+//ReFood
+
+console.log(internName);
+//John Doe
+
+console.log(intershipYear);
+//2025
+
+companyName = "ReFood Tech";
+//Reassign companyName
+
+internName = "Jane Smith";
+//Reassign internName
+//Try reassing intershipYear
+
+let age = 22;//Number
+let isActive = true;//Boolean
+let skills =["JS","HTML"];//Array
+let details = { departement :"Tech" ,level : "Intern" };//Object
+
+console.log(typeof companyName);//String
+console.log(typeof age);//Number
+console.log(typeof isActive);//Boolean
+console.log(typeof skills);//Object
+console.log(typeof details);//Object
